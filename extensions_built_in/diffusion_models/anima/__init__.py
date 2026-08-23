5427af06bad724883350f4c3f891ef456abe6c37
orangemagic123 <69315202+orangemagic123@users.noreply.github.com>
2026-08-23 09:27:51 +0900

    fix: Anima DoRA 체크포인트를 ComfyUI 형식으로 저장

--- extensions_built_in/diffusion_models/anima/__init__.py
+++ extensions_built_in/diffusion_models/anima/__init__.py
@@ -1,1 +1,18 @@
-from .anima import AnimaModel, AnimaPromptEmbeds
+from toolkit.util.dora_serialization import (
+    convert_comfy_dora_scale_to_internal,
+    convert_dora_magnitude_to_comfy,
+)
+
+from .anima import AnimaModel as _AnimaModel, AnimaPromptEmbeds
+
+
+class AnimaModel(_AnimaModel):
+    """Anima model with ComfyUI-compatible DoRA checkpoint serialization."""
+
+    def convert_lora_weights_before_save(self, state_dict):
+        state_dict = super().convert_lora_weights_before_save(state_dict)
+        return convert_dora_magnitude_to_comfy(state_dict)
+
+    def convert_lora_weights_before_load(self, state_dict):
+        state_dict = convert_comfy_dora_scale_to_internal(state_dict)
+        return super().convert_lora_weights_before_load(state_dict)
